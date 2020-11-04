4e67b8310bf2da4e47bcfda8cbf702cb9b2a3846
tusharkhandejod <tusharkhandejod@gmail.com>
2020-11-04 13:42:43 +0530

    Started flipping a coin and displaying head or tail appeared

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 echo "Welcome to the Flip Coin Combination program"
-
+tossedcoin=$(($RANDOM%2))
+if [[ $tossedcoin -eq 1 ]]
+then
+echo "Head appeared"
+elif [[ $tossedcoin -eq 0 ]]
+then
+echo "Tail appeared"
+fi
